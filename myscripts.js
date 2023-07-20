726d48a657a6103584ca117969aa1a50bade3a6a
Adrian Montenegro <adrmontenegro97@gmail.com>
2023-07-20 02:42:18 -0500

    initial commit

---
+++ myscripts.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.present').toggleClass('tada');
+    $('.title').toggleClass('tada2');
+  },2000);
+  
+  $('.toggle-tada').click(function() {
+    $('.present').toggleClass('tada');
+    $('.title').toggleClass('tada2');
+  });
